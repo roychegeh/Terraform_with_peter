5ac7257e510d3bd02aba86dcf5688a0f10e605fd
PETER CHEGE <roychegeh@gmail.com>
2026-02-06 12:11:19 +0300

    Added infra workflow

--- s3.tf
+++ s3.tf
@@ -1,10 +1,22 @@
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
 # Create S3 Bucket
 resource "aws_s3_bucket" "demo_bucket" {
-  bucket = "klaudtechie-bucket-12345"
+  bucket = "klaudtechie-bucket-${random_id.bucket_suffix.hex}"
 
   tags = {
-    Name        = "My Bucket"
+    Name        = "Vault Dynamic Secret Test"
     Environment = "Dev"
+    ManagedBy = "Terraform"
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.demo_bucket.id
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
